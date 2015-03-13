c6c7c8de6566763aa5a1572d58fdd67fd3f89cfa
Ritch08 <fritsch.nicolas@hotmail.fr>
2015-03-13 16:42:03 +0100

    Update register.php

--- register.php
+++ register.php
@@ -2,71 +2,87 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<link rel="stylesheet" href="style_sign_in.css"/>
+<link rel="stylesheet" href="style.register.css"/>
 <title>Inscription</title>
 
 </head>
+<body>
 <?php
-
+ini_set('display_errors', 'off');
 if(isset($_POST['submit']))
 {
-$username= htmlentities(trim ($_POST['username']));
-$password= htmlentities(trim($_POST['password']));
-$repeatpassword= htmlentities(trim($_POST['repeatpassword']));
-if($username&&$password&&$repeatpassword)
-{
-if ($password==$repeatpassword)
-{
-$password= md5 ($password);
-/* on crypte le mot de passe pour eviter que les pirates informatiques puissent le récupérer */
-$connect= mysql_connect('localhost','root','') or die('Error');
-mysql_select_db('phplogin');
-$reg= mysql_query("SELECT * FROM users WHERE username='$username'"); /* Cette ligne empêche la création de pseudos semblables */
-$rows = mysql_num_rows($reg);
+   $nom= htmlentities(trim ($_POST['nom']));
+   $prenom= htmlentities(trim ($_POST['prenom']));
+   $pseudodeconnexion= htmlentities(trim ($_POST['pseudodeconnexion']));
+   $motdepasse= htmlentities(trim($_POST['motdepasse']));
+   $confirmerlemotdepasse= htmlentities(trim($_POST['confirmerlemotdepasse']));
+   $adresse= htmlentities(trim ($_POST['adresse']));
+   $codepostal= htmlentities(trim ($_POST['codepostal']));
+   $ville= htmlentities(trim ($_POST['ville']));
+   $departement= htmlentities(trim ($_POST['departement']));
+   $pays= htmlentities(trim ($_POST['pays']));
+   $courriel= htmlentities(trim ($_POST['courriel']));
+   $telephone= htmlentities(trim ($_POST['telephone']));
 
-if($rows==0) /* si le pseudo n'est pas trouvé alors on l'enregistre */
-{
+     if($nom&&$pseudodeconnexion&&$motdepasse&&$confirmerlemotdepasse&&$adresse&&$codepostal&&$ville&&$courriel)
+     {
+       if ($motdepasse==$confirmerlemotdepasse)
+       {
+          $motdepasse= md5 ($motdepasse);  /* on crypte le mot de passe pour plus de sécurité */
+		  
+          $connect= mysql_connect('localhost','root','') or die('Error');
+		  
+           mysql_select_db('phplogin2');
+		   
+           $reg= mysql_query("SELECT * FROM users WHERE pseudodeconnexion='$pseudodeconnexion'"); /* Cette ligne empêche la création de pseudos semblables */
+		   
+           $rows = mysql_num_rows($reg);
 
-$query = mysql_query ("INSERT INTO users VALUES('','$username','$password')");
+           if($rows==0) /* si le pseudo n'est pas trouvé alors on l'enregistre */
+           {
 
-die("Inscription terminée");
-}else echo "Ce pseudo n'est pas disponible";
-}else echo"Les passwords ne correspondent pas";
+              $query = mysql_query ("INSERT INTO users VALUES('','$nom','$prenom','$pseudodeconnexion','$motdepasse','$adresse','$codepostal','$ville','$departement','$pays','$courriel','$telephone')");
 
-}else echo "Veuillez saisir tous les champs";
+              die("Inscription terminée <a href='login.php'> connéctez </a> vous");
+			  
+            }else echo "Ce pseudo n'est pas disponible";
+			
+        }else echo"Les mots de passe ne correspondent pas";
+
+    }else echo "Veuillez saisir tous les champs";
 }
 
 
 ?>
-<div id="bloc_page">
+     <div id="bloc_page">
             <header>
                 <div id="titre_principal">
-                    <img src="images/site_logo.jpg" alt="Logo du Site" id="logo" />
-                    <h1 id="titre_site">Adopt'UnFruit.com</h1>
+                    <img src="images/logo.png" alt="Logo du Site" id="logo" />
+                    
                     <h2>Site d'échange et de partage de fruits et légumes</h2>
                 </div>
                 
                 <nav>
                     <ul>
                         <li><a href="homepage.html">Accueil</a></li>
-                        <li><a href="inscription2.php">Mon Compte</a></li>
+                        <li><a href="login.php">Mon Compte</a></li>
                         <li><a href="#">Forum</a></li>
                         <li><a href="#">Ma Région</a></li>
-						<li><a href="#">Recherche avancée</a></li>
+    					<li><a href="#">Recherche avancée</a></li>
                     </ul>
                 </nav>
             </header>
 
 
-<body>
+
 			<div id="banniere_image">
                 
             </div>
 
-<form id="formulaire" method="POST" action="inscription2.php">
 
 
-	<form method="POST" action="register.php">
+
+	<form id="formulaire" method="POST" action="register.php">
 	<p>Nom: <input type="text" name="nom" > </p>
 	<p>Prénom: <input type="text" name="prenom" ></p>
 	<p>Pseudo de connexion: <input type="text" name="pseudodeconnexion" ></p>
@@ -83,8 +99,13 @@ die("Inscription terminée");
 	<input type="checkbox" name="conditions"></p></br>
 	<input type="submit" value="S'inscrire" name="submit">
 	</form>
-</form>
 
 </body>
 </div>
 </html>
+
+	
+	
+
+
+
